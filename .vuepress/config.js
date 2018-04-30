289fc7874c4b3bbc10c2b0a614921092bceecb8b
Peter Wang <happypeter1983@gmail.com>
2018-04-30 09:29:08 +0800

    nav

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'Vue 路径',
+  description: 'Just playing around',
+  themeConfig: {
+    search: false,
+    searchMaxSuggestions: 10,
+    nav: [{ text: '首页', link: '/' }, { text: '路径', link: '/guide/' }]
+  }
 }
